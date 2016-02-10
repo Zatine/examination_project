c80217ed16cd59437631b796b21d0939158704db
Mikaela <me@mikaelagrundin.com>
2016-02-10 20:23:45 +0100

    Keyboard navigation for form elements

--- dev/scripts/directives/forms.js
+++ dev/scripts/directives/forms.js
@@ -80,7 +80,7 @@ app.directive('formSection', function(){
                   '<div class="inline-block">',
                     '<input type="radio" id="{{id}}" name="{{name}}" value="{{value}}" ng-disabled="{{disabled}}" ng-model="model" form="{{formName}}">',
                     '<label for="{{id}}">',
-                    '<span><svg viewBox="0 0 100 100"><circle cx="50" cy="50" r="40"/></svg></span>',
+                    '<span tabindex="0"><svg viewBox="0 0 100 100"><circle cx="50" cy="50" r="40"/></svg></span>',
                     ' {{label}}',
                     '</label>',
                   '</div>'
@@ -94,10 +94,17 @@ app.directive('formSection', function(){
       },
       link: function(scope, elm, attrs, ctrl){
         elm.removeAttr('id');
+        var focused = false;
         if(ctrl[0]) scope.formName = ctrl[0].$name; scope.form = ctrl[0];
         scope.value = scope.label.replace(/ /g,"_").toLowerCase();
         if(attrs.disabled !== undefined) scope.disabled = true;
         if(attrs.checked !== undefined) scope.model = scope.value;
+        
+        elm.bind('keypress', function(event){
+          if(event.keyCode !== 13) return true;
+          scope.model = scope.value;
+          if(!scope.$$phase) scope.$apply();
+        });
       }
     }
   })
@@ -110,7 +117,7 @@ app.directive('formSection', function(){
                   '<div class="inline-block">',
                     '<input type="checkbox" id="{{id}}" name="{{name}}" value="{{value}}" ng-disabled="{{disabled}}" form="{{formName}}" ng-model="model">',
                     '<label for="{{id}}">',
-                    '<span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.66 48.75"><path d="M2.41,30.3l5.21-7.16A0.81,0.81,0,0,1,8.86,23l9.43,8a0.81,0.81,0,0,0,1.24-.15L40.9,1.37a0.81,0.81,0,0,1,1.19-.19l6.61,5a0.81,0.81,0,0,1,.17,1.06l-28,37.9a0.81,0.81,0,0,1-1.22.17l-17.12-14A0.81,0.81,0,0,1,2.41,30.3Z"/></svg></span>',
+                    '<span tabindex="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.66 48.75"><path d="M2.41,30.3l5.21-7.16A0.81,0.81,0,0,1,8.86,23l9.43,8a0.81,0.81,0,0,0,1.24-.15L40.9,1.37a0.81,0.81,0,0,1,1.19-.19l6.61,5a0.81,0.81,0,0,1,.17,1.06l-28,37.9a0.81,0.81,0,0,1-1.22.17l-17.12-14A0.81,0.81,0,0,1,2.41,30.3Z"/></svg></span>',
                     ' {{label}}',
                     '</label>',
                   '</div'
@@ -127,6 +134,13 @@ app.directive('formSection', function(){
         if(ctrl[0]) scope.formName = ctrl[0].$name; scope.form = ctrl[0];
         scope.value = scope.label.replace(/ /g,"_").toLowerCase();
         if(attrs.disabled !== undefined) scope.disabled = true;
+        scope.model = scope.model ? scope.model : false;
+        
+        elm.bind('keypress', function(event){
+          if(event.keyCode !== 13) return true;
+          scope.model = !scope.model;
+          if(!scope.$$phase) scope.$apply();
+        });
       }
     }
   })
@@ -139,17 +153,17 @@ app.directive('formSection', function(){
                   '<div ng-switch="multiple">',
                   '<div class="dropdown" ng-switch-when="false">',
                     '<select form="{{formName}}" ng-options="option as option.name for option in options" ng-model="model" id="{{id}}" name="{{name}}" ng-disabled="{{disabled}}"></select>',
-                    '<ul ng-class="ulClass">',
+                    '<ul ng-class="{\'disabled\': disabled}" tabindex="0" ng-blur="hideMenu()">',
                       '<li ng-click="toggleMenu()">',
                       '<label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.88 32.24"><polygon class="a" points="9.98 0.66 23.11 20.19 37.17 0.5 44.92 0.5 23.11 31.38 0.98 0.59 9.98 0.66"/></svg></label>',
                       '{{model.name}}</li>',
-                      '<li ng-show="menu" ng-repeat="option in options" ng-click="setModel(option)">{{option.name}}</li>',
+                      '<li ng-show="showMenu" ng-repeat="option in options" ng-click="setModel(option)" ng-class="{\'active\': model === option}">{{option.name}}</li>',
                       '</ul>',
                     '</div>',
                     '<div class="multiple" ng-switch-when="true">',
                     '<select form="{{formName}}" ng-options="option.name for option in options" ng-model="model" id="{{id}}" name="{{name}}" multiple ng-disabled="{{disabled}}"></select>',
-                    '<ul ng-class="ulClass">',
-                      '<li ng-repeat="option in options" ng-click="toggleOption(option)" ng-class="option.class">{{option.name}}</li>',
+                    '<ul ng-class="{\'disabled\': disabled}" tabindex="0" ng-blur="resetIndex()">',
+                      '<li ng-repeat="option in options" ng-click="toggleOption(option)" ng-class="{\'active\': $index === multipleIndex, \'selected\': model.indexOf(option) !== -1}">{{option.name}}</li>',
                     '</ul>',
                   '</div>'
       ].join(''),
@@ -164,25 +178,86 @@ app.directive('formSection', function(){
       link: function(scope, elm, attrs, ctrl){
         scope.multiple = false;
         if(ctrl[0]) scope.formName = ctrl[0].$name; scope.form = ctrl[0];
-        if(attrs.disabled !== undefined) {scope.disabled = true; scope.ulClass = 'disabled';}
+        if(attrs.disabled !== undefined) scope.disabled = true;
         if(attrs.multiple !== undefined) scope.multiple = true;
         scope.model = scope.multiple ? [] : (scope.model || scope.options[0]);
-        scope.menu = false;
         
-        scope.toggleMenu = function(){
-          if(scope.disabled) return;
-          scope.menu = !scope.menu;
-        };
-        scope.setModel = function(option){
-          scope.model = option;
-          scope.menu = false;
+        var navigateMenu;
+        
+        if(!scope.multiple){
+            scope.showMenu = false;
+            scope.toggleMenu = function(){
+              if(scope.disabled) return;
+              scope.showMenu = !scope.showMenu;
+            };
+            scope.hideMenu = function(){
+              scope.showMenu = false;
+            }
+            scope.setModel = function(option){
+              scope.model = option;
+              scope.showMenu = false;
+            }
+            
+            navigateMenu = function (event){
+              var index = scope.options.indexOf(scope.model);
+               //enter
+              if(event.keyCode === 13){
+                scope.toggleMenu();
+              }
+              //up
+              else if(event.keyCode === 38){
+                  if(index === 0) return;
+                  scope.model = scope.options[index - 1];
+              }
+              //down
+              else if(event.keyCode === 40){
+                if(index === scope.options.length - 1) return;
+                scope.model = scope.options[index + 1];
+              }
+            }
         }
         
-        scope.toggleOption = function(option){
-          if(scope.disabled) return;
-          if(scope.model.indexOf(option) !== -1) {scope.model.splice(scope.model.indexOf(option), 1); option.class = '';}
-          else {scope.model.push(option); option.class = 'selected';}
+        if(scope.multiple){
+          scope.toggleOption = function(option){
+            if(scope.disabled) return;
+            if(scope.model.indexOf(option) !== -1) scope.model.splice(scope.model.indexOf(option), 1);
+            else scope.model.push(option);
+          }
+
+          scope.resetIndex = function(){
+            scope.multipleIndex = undefined;
+          }
+          
+          navigateMenu = function(event){
+              //enter
+              if(event.keyCode === 13){
+                scope.toggleOption(scope.options[scope.multipleIndex]);
+              }
+              //up
+              else if(event.keyCode === 38){
+                if(scope.multipleIndex === 0) return;
+                if(scope.multipleIndex === undefined) scope.multipleIndex = scope.options.length - 1;
+                else scope.multipleIndex--;
+              }
+              //down
+              else if(event.keyCode === 40){
+                if(scope.multipleIndex === scope.options.length - 1) return;
+                if(scope.multipleIndex === undefined) scope.multipleIndex = 0;
+                else scope.multipleIndex++;
+              }
+          }
         }
+        
+        
+        elm.bind('keydown', function(event){
+          if(event.keyCode !== 38 && event.keyCode !== 40 && event.keyCode !== 13) return true;
+          
+          event.preventDefault();
+          
+          navigateMenu(event);
+          if(!scope.$$phase) scope.$apply();
+        });
+          
         
         $document.bind('click', function(event){
           elm.removeAttr('id');
