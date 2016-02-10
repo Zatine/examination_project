82e0c2d6ed6b220cf7587e96970439038a5a2b4f
Mikaela <me@mikaelagrundin.com>
2016-02-10 16:13:49 +0100

    Automatic injection of css/js-files through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
+    inject = require('gulp-inject'),
     browserSync = require('browser-sync').create();
 
 
@@ -7,6 +8,14 @@ gulp.task('sass', function () {
   return sass('dev/style/main.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('dev/style'));
+});
+
+gulp.task('inject-index', function(){
+  var target = gulp.src('./dev/index.html'),
+      sources = gulp.src(['scripts/**/*.js', 'style/*.css'], {read: false, cwd: __dirname + '/dev'});
+  
+  return target.pipe(inject(sources)) 
+         .pipe(gulp.dest('./dev'));
 });
 
 gulp.task('serve', function(){
@@ -18,8 +27,18 @@ gulp.task('serve', function(){
 
     gulp.watch("dev/style/*.scss", ['sass']);
     gulp.watch("dev/style/*.css").on('change', browserSync.reload);
+    gulp.watch("dev/style/*.css", function(event){
+      if(event.type === 'added' || event.type === 'deleted'){
+        gulp.start('inject-index');
+      }
+    });
 
     gulp.watch("dev/scripts/**/*.js").on('change', browserSync.reload);
+    gulp.watch("dev/scripts/**/*.js", function(event){
+      if(event.type === 'added' || event.type === 'deleted'){
+        gulp.start('inject-index');
+      }
+    });
 });
 
 
